0ea471b98fad6bdf445eb35226aecee424bab04f
samkhancodes <Samk.code@gmail.com>
2024-07-19 23:26:02 +0500

    sign in

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -3,8 +3,11 @@ import SignInForm from './SignInForm';
 
 const SignInPage: React.FC = () => {
   return (
-    <div className='flex h-screen justify-center items-center '>
+    <div className='flex flex-col h-screen justify-center items-center '>
       <SignInForm />
+      <p className='mt-8'>Admin Credentials:</p>
+      <p>codionslab@gmail.com</p>
+      <p>12345678</p>
     </div>
   );
 };
